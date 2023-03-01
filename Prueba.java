24792a5e20bd7dc36bf9df215142fe77b14eb882
Aguilera450 <aguilera@ciencias.unam.mx>
2023-02-28 20:01:13 -0600

    UPDATE: se anexan archivos de prueba.

---
+++ Prueba.java
@@ -0,0 +1,16 @@
+/**
+ * Esta es una clase de prueba.
+ */
+
+public class Prueba {
+    private string atr1 = " ";
+    private int atr1 = 1;
+    
+    /** Constructor por omisión. */
+    Prueba() {}
+    
+    /** Constructor por parámetros. */
+    Prueba(string str1, string str2) {
+	atr1 = str1 + " : " + str2;
+    }
+}
---
+++ Prueba.py
@@ -0,0 +1,7 @@
+class Prueba:
+    atr1 = " "
+    atr2 = 1
+
+    def __init__(self, numero):
+        atr2 = numero
+        self.stringP = ": " + numero
